57dab099ee6ed287d5ea5ee79d2f4f14ea596fcb
sudoswift <sudoswift@gmail.com>
2021-05-29 23:31:38 +0900

    add toolbar, toolBarItem, label

--- SwitUI_menu/ContentView.swift
+++ SwitUI_menu/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Text("Hello, world!")
+                .padding()
+                .navigationTitle("하위하위")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Menu(content: {
+                            Text("하하하")
+                        }, label: {
+                            // Custom Color 적용하는 방법
+                            Circle().foregroundColor(Color.init(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)))
+                            Label("더보기", systemImage: "ellipsis")
+                                .foregroundColor(.black)
+                        })
+                            }
+                        }
+        } // NavigationView
     }
 }
 
