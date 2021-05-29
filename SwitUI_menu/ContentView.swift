e0034b3f729b0a076ae7a855d4e39e682423a985
sudoswift <sudoswift@gmail.com>
2021-05-29 23:05:43 +0900

    Initial Commit

---
+++ SwitUI_menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitUI_menu
+//
+//  Created by izvern on 2021/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwitUI_menu/SwitUI_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUI_menuApp.swift
+//  SwitUI_menu
+//
+//  Created by izvern on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwitUI_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
